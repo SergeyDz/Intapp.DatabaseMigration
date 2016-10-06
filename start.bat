ff569ae595cf500891b272615338376796d1b661
SergeyDzIntapp <sergey.dziuban@intapp.com>
2016-10-06 18:55:59 +0300

    skip fixed

--- start.bat
+++ start.bat
@@ -2,4 +2,4 @@
 SET mssql-connection="jdbc:sqlserver://localhost;databaseName=POC-OMM-2;user=sa;password=Tsunami9"
 SET pgsql-connection="jdbc:postgresql://sdzyuban-pc.fg.local:5432/opendb?user=postgres&password=Tsunami9"
 
-java -jar target/Intapp.DatabaseMigration-1.0-SNAPSHOT-jar-with-dependencies.jar mssql-connection=%mssql-connection% pgsql-connection=%pgsql-connection% skip=Configs,FormAnswers,RequestDefinitions
+java -jar target/Intapp.DatabaseMigration-1.0-SNAPSHOT-jar-with-dependencies.jar mssql-connection=%mssql-connection% pgsql-connection=%pgsql-connection% skip=Configs
